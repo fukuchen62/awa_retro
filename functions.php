bf6b77b55d3b6d161eac2471e5cf8cbb757ed0dd
fukuchen62 <fukuchen62@gmail.com>
2024-03-18 14:24:25 +0900

    テーマの初期作成
    テーマの一分のテンプレートファイルを作詞した
    
    福島

---
+++ functions.php
@@ -0,0 +1,182 @@
+<?php
+
+// 管理バーを非表示させる
+// add_filter('show_admin_bar', '__return_false');
+
+
+/**
+ * テーマセットアップ後に、WordPressの初期化設定
+ *
+ * @return void
+ */
+function awaretro_after_setup_theme()
+{
+    /**
+     * <title>タぐを出力する
+     */
+    add_theme_support('title-tag');
+
+    /**
+     * アイキャッチ画像を使用可能にする
+     */
+    add_theme_support('post-thumbnails');
+
+    /**
+     * カスタムメニュー機能を使用可能にする
+     */
+    add_theme_support('menus');
+}
+add_action('after_setup_theme', 'awaretro_after_setup_theme');
+
+/**
+ * スタイルシートとJSファイルを読み込む関数
+ *
+ * @return void
+ */
+function awaretro_wp_enqueue_scripts()
+{
+    // WEBサイトの共通のスタイルシート、JSファイルを読み込む
+    // スタイルファイルを読み込む
+    wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css');
+
+    wp_enqueue_style('google-web-fonts', 'https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap');
+
+    // リセットCSS
+    wp_enqueue_style('awaretro-reset', get_template_directory_uri() . '/assets/css/reset.css');
+
+    // 共通のスタイルシート
+    wp_enqueue_style('awaretro-app', get_template_directory_uri() . '/assets/css/app.css');
+
+    // jQueryを読み込む
+    wp_enqueue_script('jquery');
+
+    // JSファイルを読み込む
+    wp_enqueue_script(
+        'awaretro-main',
+        get_template_directory_uri() . '/assets/js/main.js',
+        '',
+        '',
+        false  //フッターに読み込むように
+    );
+
+    // それぞれのページに必要とするスタイルシート、JSファイルを読み込む
+
+    if (is_home()) {
+        // トップページのKVのスライドショー
+        // CSSファイルを読み込む
+        wp_enqueue_style('slick-carousel', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css');
+
+        // JSファイルを読み込む
+        wp_enqueue_script(
+            'slick-carousel',
+            'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js',
+            '',
+            '',
+            true
+        );
+        // JSファイルを読み込む
+        wp_enqueue_script(
+            'awaretro-home',
+            get_template_directory_uri() . '/assets/js/home.js',
+            '',
+            '',
+            true
+        );
+    } else if (is_page()) {
+        // 固定ページ
+
+    } else if (is_category()) {
+        // カテゴリーページ
+
+    } else if (is_tax()) {
+        // タクソノミーページ
+
+    } else if (is_single()) {
+        // 汎用のsingleページ
+
+    } else if (is_singular('food')) {
+        // 特定のfoodの詳細ページ
+
+    }
+}
+add_action('wp_enqueue_scripts', 'awaretro_wp_enqueue_scripts');
+// add_action('読みだすタイミング', '呼び出す関数名');
+
+
+/**
+ * タイトルの区切り文字を設定する
+ *
+ * @param string $separator
+ * @return string
+ */
+function awaretro_document_title_separator(string $separator): string
+{
+    $separator = '|';     //好きな文字をセットする
+    return $separator;
+}
+
+// add_filter('呼び出すタイミング', '呼びだす関数名');
+add_filter(
+    'document_title_separator',
+    'awaretro_document_title_separator'
+);
+
+/**
+ * Contact Form 7の時には整形機能をOFFにする
+ *
+ * @return void
+ */
+function awaretro_wpcf7_autop()
+{
+    return false;
+}
+add_filter('wpcf7_autop_or_not', 'awaretro_wpcf7_autop');
+
+/**
+ * メインクエリを調整する
+ *
+ * @param [type] $query
+ * @return void
+ */
+function awaretro_pre_get_posts($query)
+{
+    // 管理画面、またはメインクエリでないとき対象外とする
+    if (is_admin() || !$query->is_main_query()) {
+        return;
+    }
+
+    if ($query->is_home()) {
+        // トップページの場合は変更する
+        // $query->set('posts_per_page', 3);       //3件だけ
+        // $query->set('category_name', 'news');   //カテゴリーnewsだけ
+    } else if (is_search()) {
+        //検索の場合は、投稿のみ対象とする
+        // $query->set('post_type', 'post');
+    }
+
+    //公開する投稿だけ
+    $query->set('post_status', 'publish');
+    return;
+}
+// アクションフックで関数を呼び出す
+add_action('pre_get_posts', 'awaretro_pre_get_posts');
+
+
+/**
+ * descriptionを取得して出力関数
+ *
+ * @return void
+ */
+function fst_description()
+{
+    // トップページの場合は
+    if (is_home()) {
+        // トップページ
+        $description = "トップページのデスクリプションです。";
+    } else {
+        // その他の下層ページ
+        $description = "下層ページのデスクリプションです。";
+    }
+    echo $description;
+    return;
+}
---
+++ index.php
@@ -0,0 +1,16 @@
+<!-- header.phpを読み込む -->
+<?php
+get_header();
+?>
+
+
+<main>
+    <section>
+        <h2>インデックステンプレートファイルです。</h2>
+    </section>
+</main>
+
+<!-- footer.phpを読み込む -->
+<?php
+get_footer();
+?>
