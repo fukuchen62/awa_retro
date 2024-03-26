15fbd91fe550f38e3040b2f749809b090014335d
qlip-sk07 <qlip.user07@gmail.com>
2024-03-26 14:35:01 +0900

    ギャラリーjs
    コメントアウトしたJSを追加
    
    妹尾

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -7,3 +7,21 @@ $(function () {
 
     });
 });
+
+
+// スクロール抑制
+// function noscroll(e) {
+//     e.preventDefault();
+// }
+
+// .gallery_itemをクリックしたときに動きを止める
+// $('.gallery_item').on('click', function () {
+//     document.addEventListener('touchmove', noscroll, { passive: false });
+//     document.addEventListener('wheel', noscroll, { passive: false });
+// });
+
+// .delをクリックしたときにスクロールストップを止める
+// $('.del').on('click', function () {
+//     document.removeEventListener("wheel", noscroll, { passive: false });
+//     document.removeEventListener("touchmove", noscroll, { passive: false });
+// });
