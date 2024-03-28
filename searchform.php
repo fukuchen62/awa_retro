9dfd8b5c8f7a7c653530aaca58d13f007f3e3e32
nokio1031 <nokio1031@gmail.com>
2024-03-28 16:38:37 +0900

    検索ページ編集中
    検索ページ編集中
    
    松田典子

--- searchform.php
+++ searchform.php
@@ -15,36 +15,35 @@
                         <input type="text" name="word" class="search_word_input_data">
                     </div>
                     <!--  -->
-                    <div class="search_category_title">カテゴリーで調べる</div>
-                    <div class="search_form_title">お食事処で調べる</div>
+                    <div class="search_form_title">地域</div>
                     <div class="search_form_input">
-                        <label class="search_form_label"><input type="checkbox" name="food" value="喫茶店・カフェ">喫茶店・カフェ</label>
-                        <label class="search_form_label"><input type="checkbox" name="food" value="ご飯">ご飯</label>
-                        <label class="search_form_label"><input type="checkbox" name="food" value="甘味処">甘味処</label>
+                        <label class="search_form_label"><input type="checkbox" name="region[]" value="東部">東部</label>
+                        <label class="search_form_label"><input type="checkbox" name="region[]" value="西部">西部</label>
+                        <label class="search_form_label"><input type="checkbox" name="region[]" value="南部">南部</label>
+                        <label class="search_form_label"><input type="checkbox" name="region[]" value="徳島市">徳島市</label>
                     </div>
                     <!--  -->
-                    <div class="search_form_title">スポットで調べる</div>
+                    <div class="search_form_title">お食事処</div>
                     <div class="search_form_input">
-                        <label class="search_form_label"><input type="checkbox" name="spot" value="自動販売機">自動販売機</label>
-                        <label class="search_form_label"><input type="checkbox" name="spot" value="お店">お店</label>
-                        <label class="search_form_label"><input type="checkbox" name="spot" value="お店">お店</label>
-                        <label class="search_form_label"><input type="checkbox" name="spot" value="お店">お店</label>
+                        <label class="search_form_label"><input type="checkbox" name="food[]" value="喫茶店・カフェ">喫茶店・カフェ</label>
+                        <label class="search_form_label"><input type="checkbox" name="food[]" value="ご飯">ご飯</label>
+                        <label class="search_form_label"><input type="checkbox" name="food[]" value="甘味処">甘味処</label>
                     </div>
                     <!--  -->
-                    <div class="search_form_title">用途で調べる</div>
+                    <div class="search_form_title">スポット</div>
                     <div class="search_form_input">
-                        <label class="search_form_label"><input type="checkbox" name="purpose" value="食べる">食べる</label>
-                        <label class="search_form_label"><input type="checkbox" name="purpose" value="見る">見る</label>
-                        <label class="search_form_label"><input type="checkbox" name="purpose" value="遊ぶ">遊ぶ</label>
-                        <label class="search_form_label"><input type="checkbox" name="purpose" value="デート">デート</label>
+                        <label class="search_form_label"><input type="checkbox" name="spot[]" value="自動販売機">自動販売機</label>
+                        <label class="search_form_label"><input type="checkbox" name="spot[]" value="お店">お店</label>
+                        <label class="search_form_label"><input type="checkbox" name="spot[]" value="お店">お店</label>
+                        <label class="search_form_label"><input type="checkbox" name="spot[]" value="お店">お店</label>
                     </div>
                     <!--  -->
-                    <div class="search_form_title">地域で調べる</div>
+                    <div class="search_form_title">用途</div>
                     <div class="search_form_input">
-                        <label class="search_form_label"><input type="checkbox" name="region" value="東部">東部</label>
-                        <label class="search_form_label"><input type="checkbox" name="region" value="西部">西部</label>
-                        <label class="search_form_label"><input type="checkbox" name="region" value="南部">南部</label>
-                        <label class="search_form_label"><input type="checkbox" name="region" value="徳島市">徳島市</label>
+                        <label class="search_form_label"><input type="checkbox" name="purpose[]" value="食べる">食べる</label>
+                        <label class="search_form_label"><input type="checkbox" name="purpose[]" value="見る">見る</label>
+                        <label class="search_form_label"><input type="checkbox" name="purpose[]" value="遊ぶ">遊ぶ</label>
+                        <label class="search_form_label"><input type="checkbox" name="purpose[]" value="デート">デート</label>
                     </div>
                     <div class="search_form_post">
                         <div class="bs"><input type="submit" value="検索する"></div>
@@ -66,144 +65,5 @@
                 </div>
             </div>
         </div>
-        <div class="search_count_box">
-            <div class="search_count_box_text">結果：「〇〇件」</div>
-        </div>
-
-    </section>
-</div>
-
-<div class="container">
-    <!-- 周辺のスポット ========================== -->
-    <section id="card_list_container" class="card_list_container">
-        <div class="card_list_box">
-
-            <!-- カード ============ -->
-            <div class="card_list_content">
-
-                <!-- カード全体をhoverした時のリンク -->
-                <a href="#" tabindex="-1"></a>
-
-                <!-- 内側枠(飾り) -->
-                <div class="card_list_content_inner"></div>
-
-                <!-- カード画像 -->
-                <div class="card_list_contant_img">
-                    <img src="../uploads/test_img1.png" alt="周辺スポットの写真">
-                </div>
-
-                <!-- カードタグ -->
-                <div class="card_list_contant_tag">
-                    <a href="#">喫茶店</a>
-                    <a href="#">洋食</a>
-                    <a href="#">ナポリタン</a>
-                    <a href="#">クリームソーダ</a>
-                    <a href="#">食べる</a>
-                </div>
-
-
-                <!-- カードタイトル -->
-                <div class="card_list_content_name">
-                    <h3>ギャラリー喫茶グレイス</h3>
-                </div>
-
-                <!-- カードテキスト -->
-                <div class="card_list_contant_text">
-                    <p>
-                        ここにはお店の説明文が入ります。
-                        ここにはお店の説明文が入ります。
-                        ここにはお店の説明文が入ります。
-                    </p>
-                </div>
-
-            </div>
-
-            <!-- カード ============ -->
-            <div class="card_list_content">
-
-                <!-- カード全体をhoverした時のリンク -->
-                <a href="#" tabindex="-1"></a>
-
-                <!-- 内側枠(飾り) -->
-                <div class="card_list_content_inner"></div>
-
-                <!-- カード画像 -->
-                <div class="card_list_contant_img">
-                    <img src="../uploads/dummy.jpg" alt="周辺スポットの写真">
-                </div>
-
-                <!-- カードタグ -->
-                <div class="card_list_contant_tag">
-                    <a href="#">喫茶店</a>
-                    <a href="#">洋食</a>
-                    <a href="#">ナポリタン</a>
-                    <a href="#">クリームソーダ</a>
-                    <a href="#">食べる</a>
-                </div>
-
-
-                <!-- カードタイトル -->
-                <div class="card_list_content_name">
-                    <h3>ギャラリー喫茶グレイス</h3>
-                </div>
-
-                <!-- カードテキスト -->
-                <div class="card_list_contant_text">
-                    <p>
-                        ここにはお店の説明文が入ります。
-                        ここにはお店の説明文が入ります。
-                        ここにはお店の説明文が入ります。
-                        ここにはお店の説明文が入ります。
-                    </p>
-                </div>
-
-            </div>
-
-            <!-- カード ============ -->
-            <div class="card_list_content">
-
-                <!-- カード全体をhoverした時のリンク -->
-                <a href="#" tabindex="-1"></a>
-
-                <!-- 内側枠(飾り) -->
-                <div class="card_list_content_inner"></div>
-
-                <!-- カード画像 -->
-                <div class="card_list_contant_img">
-                    <img src="../uploads/dummy.jpg" alt="周辺スポットの写真">
-                </div>
-
-                <!-- カードタグ -->
-                <div class="card_list_contant_tag">
-                    <a href="#">喫茶店</a>
-                    <a href="#">洋食</a>
-                    <a href="#">ナポリタン</a>
-                    <a href="#">クリームソーダ</a>
-                    <a href="#">食べる</a>
-                </div>
-
-
-                <!-- カードタイトル -->
-                <div class="card_list_content_name">
-                    <h3>ギャラリー喫茶グレイス <br>昭和53年創業</h3>
-                </div>
-
-                <!-- カードテキスト -->
-                <div class="card_list_contant_text">
-                    <p>
-                        ここにはお店の説明文が入ります。
-                        ここにはお店の説明文が入ります。
-                        ここにはお店の説明文が入ります。
-                        ここにはお店の説明文が入ります。
-                        ここにはお店の説明文が入ります。
-                    </p>
-                </div>
-            </div>
-
-        </div>
-        <div class="pagination_box">
-            <div>1/3 次へ→</div>
-        </div>
-
     </section>
 </div>
